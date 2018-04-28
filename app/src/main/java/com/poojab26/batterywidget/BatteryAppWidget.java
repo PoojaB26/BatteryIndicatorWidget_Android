9659206dbce4dd716838550fce46ea8409d9b09a
PoojaB26 <pbhaumik26@gmail.com>
2018-04-28 11:56:35 +0530

    Initial commit

---
+++ app/src/main/java/com/poojab26/batterywidget/BatteryAppWidget.java
@@ -0,0 +1,49 @@
+package com.poojab26.batterywidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+/**
+ * Created by poojab26 on 28-Apr-18.
+ */
+
+public class BatteryAppWidget extends AppWidgetProvider {
+
+    static String mBatteryPerc;
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.battery_app_widget);
+        views.setTextViewText(R.id.tvBatteryPerc, mBatteryPerc);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+
+    public static void setBatteryLevel(String batteryPerc){
+        mBatteryPerc = batteryPerc;
+
+    }
+
+}
---
+++ app/src/main/java/com/poojab26/batterywidget/MainActivity.java
@@ -0,0 +1,65 @@
+package com.poojab26.batterywidget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver mReceiver;
+    int battery_level;
+    String TAG = "Battery";
+    ProgressBar progressBar;
+    TextView tvBattery;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mReceiver = new BatteryBroadcastReceiver();
+        progressBar = findViewById(R.id.progressBar);
+        tvBattery = findViewById(R.id.tvBattery);
+
+    }
+
+    @Override
+    protected void onStart() {
+        registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+        unregisterReceiver(mReceiver);
+        super.onStop();
+    }
+
+    public void setValueToWidget() {
+        BatteryAppWidget.setBatteryLevel(battery_level+"%");
+
+        Intent widgetIntent = new Intent(MainActivity.this, BatteryAppWidget.class);
+        widgetIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+        int[] ids = AppWidgetManager.getInstance(getApplication())
+                .getAppWidgetIds(new ComponentName(getApplication(), BatteryAppWidget.class));
+        widgetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        sendBroadcast(widgetIntent);    }
+
+
+    private class BatteryBroadcastReceiver extends BroadcastReceiver {
+        private final static String BATTERY_LEVEL = "level";
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            battery_level = intent.getIntExtra(BATTERY_LEVEL, 0);
+           setValueToWidget();
+           tvBattery.setText(battery_level+"");
+           progressBar.setProgress(battery_level);
+        }
+    }
+}
